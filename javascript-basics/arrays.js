8bae86f6fb9c1d1d6176a78a2c882d78e68c00eb
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2025-08-20 22:03:02 +0530

    arrays javascript

--- javascript-basics/arrays.js
+++ javascript-basics/arrays.js
@@ -1,4 +1,4 @@
-const hobbies = ['sports','cooking'];
+const hobbies = ['sports','cooking','dancing'];
 for (let hobby of hobbies){
     console.log(hobby);
 }
@@ -7,5 +7,11 @@ console.log(hobbies.map(hobby => 'hobby:' + hobby));
 console.log(hobbies);
 
 hobbies.push('programming')
+hobbies.unshift('reading');
+console.log('After adding elements');
+console.log(hobbies);
 
+hobbies.pop();
+hobbies.shift();
+console.log('After removing elements');
 console.log(hobbies);
