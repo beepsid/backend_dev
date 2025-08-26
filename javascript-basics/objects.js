4a1a54d09820f596cf800e6f3ac2bcb34e193377
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2025-08-26 16:50:09 +0530

    objects in javascript

--- javascript-basics/objects.js
+++ javascript-basics/objects.js
@@ -2,7 +2,7 @@ const person ={
     name: 'sid',
     age: 21,
     greet(){
-        console.log('Hi I am ' + this.name);
+        console.log('Hi I am ' + this.name + ' and i am ' + this.age + ' years old.');
     }
 };
 
